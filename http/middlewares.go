472e6901fdec37ac817a193c6092b21f732f0b33
Ernar Khanapin <ernar.k@densertech.com>
2025-09-20 16:09:26 +0500

    fix: cors

--- http/middlewares.go
+++ http/middlewares.go
@@ -21,7 +21,7 @@ func (m *CORSMiddleware) Handle(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		// w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
