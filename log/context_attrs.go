77ec87fe36a31fda9581067daddda7ab938bfaf1
Rasikrr <rs.t.95@mail.ru>
2025-12-04 20:45:42 +0500

    created context package with ctx keys. TODO: fix previous breadcrumbs in logger

--- log/context_attrs.go
+++ log/context_attrs.go
@@ -2,23 +2,19 @@ package log
 
 import (
 	"context"
+
+	coreCtx "github.com/Rasikrr/core/context"
+
 	"log/slog"
-)
-
-type ctxKey string
-
-const (
-	CtxKeyRequestID ctxKey = "request_id"
-	CtxKeyUserID    ctxKey = "user_id"
 )
 
 func getAttrsFromCtx(ctx context.Context) []slog.Attr {
 	var attrs []slog.Attr
-	if reqID, ok := ctx.Value(CtxKeyRequestID).(string); ok {
-		attrs = append(attrs, slog.String("request_id", reqID))
+	if reqID, ok := coreCtx.RequestID(ctx); ok {
+		attrs = append(attrs, slog.String(string(coreCtx.CtxKeyRequestID), reqID))
 	}
-	if userID, ok := ctx.Value(CtxKeyUserID).(string); ok {
-		attrs = append(attrs, slog.String("user_id", userID))
+	if userID, ok := coreCtx.UserID(ctx); ok {
+		attrs = append(attrs, slog.String(string(coreCtx.CtxKeyUserID), userID))
 	}
 	return attrs
 }
