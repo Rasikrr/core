09e155fe062df358cb8efd8c6de103ec5cba9603
Rasikrr <rs.t.95@mail.ru>
2025-12-26 15:09:28 +0500

    add traceID if tracing disabled

--- http/tracing.go
+++ http/tracing.go
@@ -5,25 +5,41 @@ import (
 
 	coreCtx "github.com/Rasikrr/core/context"
 	"github.com/Rasikrr/core/tracing"
+	"github.com/google/uuid"
 	"github.com/riandyrn/otelchi"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func (s *Server) setupTracingMiddleware() {
-	if !tracing.Enabled() {
+	if tracing.Enabled() {
+		s.router.Use(otelchi.Middleware(s.name))
 		return
 	}
-	s.router.Use(otelchi.Middleware(s.name), traceCtxMiddleware)
+	s.router.Use(traceCtxMiddleware)
 }
 
 func traceCtxMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+		var traceID string
 
+		// Приоритет 1: traceID из OpenTelemetry span (если трейсинг включен)
 		sc := trace.SpanContextFromContext(ctx)
 		if sc.HasTraceID() {
-			ctx = coreCtx.WithTraceID(ctx, sc.TraceID().String())
+			traceID = sc.TraceID().String()
 		}
+
+		// Приоритет 2: traceID из заголовка запроса
+		if traceID == "" {
+			traceID = r.Header.Get(TraceIDHeader)
+		}
+
+		// Приоритет 3: генерируем новый UUID
+		if traceID == "" {
+			traceID = uuid.New().String()
+		}
+
+		ctx = coreCtx.WithTraceID(ctx, traceID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
